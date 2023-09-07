efb5f5776b3ebb6c53df69a0c86cd47fc062519c
Kumar <Santosh_Kumar39@Dell.com>
2023-09-07 14:30:37 +0530

    [Practice]: Adding New Stack class Editted

--- com/icode/QueueUsingStack.java
+++ com/icode/QueueUsingStack.java
@@ -1,2 +1,47 @@
-package com.icode;public class QueueUsingStack {
+package com.icode;
+
+
+import java.util.Stack;
+
+// 1 , 2 -> 1 2 -
+
+// 1 2 3-  stack1 - 3 - stack2 - 3,1,2 3
+
+
+class MyQueue {
+
+
+    private Stack<Integer> stack1 = new Stack<>();
+     private Stack<Integer> stack2 = new Stack<>();
+
+    public Integer deque() {
+
+        if (stack2.isEmpty()) {
+            while(!stack1.isEmpty()) {
+                stack2.add(stack1.pop()); // 1, 2
+            }
+        }
+
+        return stack2.pop();
+    }
+
+    public void enque(Integer element) {
+        stack1.add(element);
+    }
+}
+public class QueueUsingStack {
+
+    public static void main(String[] args) {
+
+        MyQueue myQueue = new MyQueue();
+        myQueue.enque(1);
+        myQueue.enque(2);
+        myQueue.enque(3);
+
+        System.out.println(myQueue.deque());
+        System.out.println(myQueue.deque());
+        System.out.println(myQueue.deque());
+        myQueue.enque(4);
+        System.out.println(myQueue.deque());
+    }
 }
